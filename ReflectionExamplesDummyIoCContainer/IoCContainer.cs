79a2bd2da474237dd18b142184261c573265a8e0
Ivan Rusev <Ivan.Rusev@indeavr.com>
2024-10-01 11:59:28 +0300

    degsp

--- ReflectionExamplesDummyIoCContainer/IoCContainer.cs
+++ ReflectionExamplesDummyIoCContainer/IoCContainer.cs
@@ -83,6 +83,8 @@
                  })
                  .ToArray();
 
+            var ivan = new List<double>();
+
             return (TContract)Activator.CreateInstance(implementationType, constructorParameters);
         }
     }
